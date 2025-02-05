b21e6fe2124c294c2312d367c8b86db9f2fd6f15
unknown <luis333joao333@gmail.com>
2025-02-05 11:17:38 -0300

    up

--- atv Banco de Dados/atv list 8 bd.sql
+++ atv Banco de Dados/atv list 8 bd.sql
@@ -1,3 +1,4 @@
+
 -- 1. Encontrar o nome e valor de todos os produtos com preço superior a 10.0.
 
 SELECT	nome , preco
@@ -8,21 +9,20 @@ WHERE	preco > 10;
 -- 2.Listar a data de todos os pedidos feitos por um cliente específico, identificado pelo cliente
 -- com id = 2.
 
-SELECT	pe.data_pedido
-FROM	clientes c , pedidos pe
+SELECT	p.data_pedido
+FROM	clientes c , pedidos p
 WHERE	c.id_cliente = 2 AND
-		c.id_cliente = pe.id_cliente;
+		c.id_cliente = p.id_cliente;
+
 
 
 -- SUM, AVG, MIN, MAX, COUNT
 -- 3. Listar a quantidade de pedidos realizados pelo cliente com id = 3
 
-SELECT	COUNT (*)
-FROM	clientes c , pedidos pe
+SELECT	COUNT(*)
+FROM	pedidos pe , clientes c
 WHERE	c.id_cliente = 3 AND
 		c.id_cliente = pe.id_cliente;
-
-
 
 
 
@@ -32,10 +32,10 @@ WHERE	c.id_cliente = 3 AND
 -- pedido específico (id_pedido = 1)
 
 
-SELECT	pro.nome , pp.quantidade , pp.preco_unitario
+SELECT	pro.nome , pp.quantidade , pp.preco_unitario 
 FROM	pedidos_produtos pp , produtos pro
 WHERE	pp.id_pedido = 1 AND
-		pp.id_produto = pro.id_produto;
+		pro.id_produto = pp.id_produto;
 
 
 
@@ -45,9 +45,10 @@ WHERE	pp.id_pedido = 1 AND
 -- 5. Listar a quantidade de pedidos realizados pelo cliente com nome = ‘Beltrano’.
 
 
-SELECT
-FROM
-WHERE
+SELECT	COUNT(p.id_pedido)
+FROM	clientes c , pedidos p
+WHERE	c.nome = 'Beltrano' AND
+		c.id_cliente = p.id_cliente;
 
 
 
